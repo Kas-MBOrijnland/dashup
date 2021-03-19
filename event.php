38dfad429d62e8c95fcfa54f8c835a2d83ed5929
Morgan <74530968+morganMBO@users.noreply.github.com>
2021-03-19 13:03:47 +0100

    update

--- event.php
+++ event.php
@@ -1,24 +1,10 @@
-<?php
-
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "energy";
-
-$conn = new mysqli($servername, $username, $password, $database);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connection_error);
-}
-<?>
-
 <!DOCTYPE html>
 <html lang="nl">
   <head>
     <meta charset="UTF-8">
     <meta http-equiv="language" content="nl">
     <meta name="description" content="DashUp Energy, ">
-    <meta name="keywords" content="DashUp Energy, ">
+    <meta name="keywords" content="DashUp Energy, Energy drink ">
     <meta name="author" content="Kas Kozakiewicz, Morgan Nicole, Florin Burlacioiu">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>DashUp Energy</title>
@@ -47,8 +33,19 @@ if ($conn->connect_error) {
   <main>
     <article id="data">
 <h1>Evenement</h1>
-
     <?php
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "energy";
+
+    $conn = new mysqli($servername, $username, $password, $database);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connection_error);
+    }
+
 
     $sql = "SELECT * FROM evenementen";
     if($result = $conn->query($sql)) {
