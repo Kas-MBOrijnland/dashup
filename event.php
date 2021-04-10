e6d179a6ebda0afcc1b3b3fbab6a7b420a708c0b
Kas-MBOrijnland <74531912+Kas-MBOrijnland@users.noreply.github.com>
2021-04-10 22:42:13 +0200

    YES

--- event.php
+++ event.php
@@ -55,10 +55,11 @@
 //WHERE datum <= NOW()
     $sql = "SELECT  DATE_FORMAT(datum, '%d-%m-%Y') AS datum, plaatsnaam
         FROM evenementen 
+        LEFT JOIN artiesten ON evenementen.artiest_id = artiesten.artiest_id
         LEFT JOIN  locaties ON evenementen.locatie_id = locaties.locatie_id";
     if($result = $conn->query($sql)) {
        while($row = $result->fetch_object()) {
-        echo "<section class='evenementen'><a href='event-page.php?id=".$row->evenement_id."'>".$row->datum.$row->plaatsnaam."</a></section>";
+        echo "<section class='evenementen'><a href='event-page.php?id='>".$row->datum." ".$row->plaatsnaam."</a></section>";
        }
             
         $result->close();
--- index.php
+++ index.php
@@ -92,11 +92,15 @@
           die("Connection failed: " . $conn->connection_error);
         }
 
-          //WHERE datum <= NOW() CAST(DateToRemoveTime as DATE)
-        $sql = "SELECT date_format(datum, '%d-%m-%Y') AS datum FROM evenementen LEFT JOIN artiesten ON evenementen.artiest_id = artiesten.artiest_id LIMIT 3";
+          //WHERE datum <= NOW()
+        $sql = "SELECT datum, plaatsnaam, date_format(datum, '%d-%m-%Y') AS datum 
+                FROM evenementen 
+                LEFT JOIN locaties ON evenementen.locatie_id = locaties.locatie_id
+                LEFT JOIN artiesten ON evenementen.artiest_id = artiesten.artiest_id
+                LIMIT 3";
         if($result = $conn->query($sql)) {
           while($row = $result->fetch_object()) {
-            echo "<section class='evenementen'><a href='event-page.php?id=".$row->evenement_id."'>".$row->datum."</a></section>";
+            echo "<section class='evenementen'><a href='event-page.php?id='>".$row->datum." ".$row->plaatsnaam."</a></section>";
           }
         
           $result->close();
