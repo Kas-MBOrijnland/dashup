b301436a8748995e891f991fea4147c2774b6c4e
Kas-MBOrijnland <74531912+Kas-MBOrijnland@users.noreply.github.com>
2021-04-10 17:28:32 +0200

    d m y

--- index.php
+++ index.php
@@ -93,7 +93,7 @@
         }
 
           //WHERE datum <= NOW() CAST(DateToRemoveTime as DATE)
-        $sql = "SELECT date_format(datum, '%m-%d-%Y') AS datum FROM evenementen LEFT JOIN artiesten ON evenementen.artiest_id = artiesten.artiest_id LIMIT 3";
+        $sql = "SELECT date_format(datum, '%d-%m-%Y') AS datum FROM evenementen LEFT JOIN artiesten ON evenementen.artiest_id = artiesten.artiest_id LIMIT 3";
         if($result = $conn->query($sql)) {
           while($row = $result->fetch_object()) {
             echo "<section class='evenementen'><a href='event-page.php?id=".$row->evenement_id."'>".$row->datum."</a></section>";
