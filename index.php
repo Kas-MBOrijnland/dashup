32f0a18d8636f5304b5857424e9262d39a3b7b9f
Kas-MBOrijnland <74531912+Kas-MBOrijnland@users.noreply.github.com>
2021-04-11 19:05:19 +0200

    as

--- index.php
+++ index.php
@@ -81,43 +81,67 @@
       <section id="base">
         <?php
 
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $database = "energy";
+          $servername = "localhost";
+          $username = "root";
+          $password = "";
+          $database = "energy";
 
-        $conn = new mysqli($servername, $username, $password, $database);
+          $conn = new mysqli($servername, $username, $password, $database);
 
-        if ($conn->connect_error) {
-          die("Connection failed: " . $conn->connection_error);
-        }
+          if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connection_error);
+          }
 
         
           //WHERE datum >= NOW()
-        $sql = "SELECT datum, plaatsnaam, date_format(datum, '%d-%m-%Y') AS datum, evenement_id
+          $sql = "SELECT datum, plaatsnaam, date_format(datum, '%d-%m-%Y') AS datum, evenement_id
                 FROM evenementen 
                 LEFT JOIN locaties ON evenementen.locatie_id = locaties.locatie_id
                 LEFT JOIN artiesten ON evenementen.artiest_id = artiesten.artiest_id
                 ORDER BY  DATE_FORMAT(datum >= NOW(), '%d-%m-%Y') ASC
                 LIMIT 3";
-        if($result = $conn->query($sql)) {
-          while($row = $result->fetch_object()) {
-            echo "<section class='evenementen'><a href='event-page.php?id=".$row->evenement_id."'>".$row->datum." ".$row->plaatsnaam."</a></section>";
-          }
+          if($result = $conn->query($sql)) {
+            while($row = $result->fetch_object()) {
+              echo "<section class='evenementen'><a href='event-page.php?id=".$row->evenement_id."'>".$row->datum." ".$row->plaatsnaam."</a></section>";
+            }
         
-          $result->close();
-        } else {
-          echo "doei";
-        }
+            $result->close();
+          } else {
+            echo "doei";
+          }
 
-        $conn->close();
+          $conn->close();
 
         ?>
 
+        <?php
+          $servername = "localhost";
+          $username = "root";
+          $password = "";
+          $database = "energy";
 
-        <article id="onsale">
+          $conn = new mysqli($servername, $username, $password, $database);
 
-        </article>
+          if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connection_error);
+          }
+          $sql = "SELECT *
+                  FROM aanbiedingen        
+                  ORDER BY  DATE_FORMAT(begindatum >= NOW(), '%d-%m-%Y') ASC
+                  LIMIT 2";
+          if($result = $conn->query($sql)) {
+            while($row = $result->fetch_object()) {
+              echo "<section class='aanbiedingen-now'><a href='aanbiedingen.html?id='>".$row->titel." ".$row->omschrijving."</a></section>";
+            }
+
+            $result->close();
+          } else {
+            echo "doei";
+          }
+
+          $conn->close();
+
+        ?>
 
       </section>
 
